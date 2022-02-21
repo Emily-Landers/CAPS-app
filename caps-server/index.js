1d5153a28d3171f3682ccdca01ddfb8786f87555
Emily Landers <emily.j.landers@gmail.com>
2022-02-20 22:35:24 -0500

    lab 13

--- caps-server/index.js
+++ caps-server/index.js
@@ -1,34 +1,56 @@
 'use strict';
 
+const uuid = require('uuid').v4;
 const socketio = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-
-const server = socketio(PORT); 
+const server = socketio(3000);
 const caps = server.of('/caps');
 
+const queue = {
+  orders: {},
+  backlog: {},
+  addOrder: function(order) {
+    let id = uuid();
+    this.orders[id] = order;
+    return id;
+  },
+  removeOrder: function (id) {
+    delete this.orders[id];
+  },
+  addBacklog: function (message, id) {
+    this.backlog[id] = message;
+  },
+};
 
-caps.on('connection', (socket) =>  {
-  console.log('socket connected');
+caps.on('connection', (socket) => {
 
-  socket.on('join', room => {
-    socket.join(room);
+  socket.on('join', orderId => {
+    socket.join(orderId);
+    caps.emit('join', orderId);
   });
 
-  socket.on('pickup', payload => {
-
-    socket.broadcast.emit('pickup', payload);
+  socket.on('pickup', (payload) => {
+    let id = queue.addOrders(payload);
+    caps.emit('pickup', { id, payload }); // send something our to caps clients
+    logger('pickup', payload);
   });
-  socket.on('in-transit', payload => {
-    // run logger
-    socket.emit('in-transit', payload);
+  socket.on('in-transit', (payload) => {
+    caps.to(payload.orderId).emit('in-transit', payload);
+    logger('in-transit', payload);
   });
   socket.on('delivery', payload => {
-    // remove package from cache??
-    socket.broadcast.emit('delivery', payload);
+    console.log(queue);
+    queue.removeOrders(payload.id);
+    caps.to(payload.orderId).emit('delivery', payload);
+    logger('delivery', payload);
   });
+  
 });
 
-function logger() {
-
+function logger(event, payload) {
+  let timestamp = new Date();
+  console.log('EVENT: ' + event, 'updatedAt: ', + timestamp);
+  console.log(payload);
+  let id = uuid();
+  queue.addBacklog('EVENT: ' + event, id);
 }
